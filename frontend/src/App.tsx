ae31ba1fcad4c30f05e28db4a1b4c8c9f680f0e8
bencahoon01 <bencahoon01@gmail.com>
2025-10-04 13:07:17 -0400

    refactored code to display testpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,7 @@
-import { Canvas } from '@react-three/fiber';
-import { useState } from 'react';
-import { Scene } from './components/canvas/Scene';
-import { InfoPanel } from './components/ui/InfoPanel';
+import TestPage from './components/pages/testPage';
 
 function App() {
-  // State to control the visibility of the UI panel
-  const [activeStation, setActiveStation] = useState<string | null>(null);
-
-  return (
-    <>
-      {/* 2D UI Layer */}
-      {activeStation && (
-        <InfoPanel
-          title={activeStation}
-          onClose={() => setActiveStation(null)}
-        />
-      )}
-
-      {/* 3D Canvas Layer */}
-      <Canvas
-        camera={{
-          fov: 75,
-          position: [0, 0, 10],
-        }}
-      >
-        <Scene />
-        {/* Example of a clickable object to test the UI */}
-        <mesh position={[-3, 0, 0]} onClick={() => setActiveStation('Biology Station')}>
-          <boxGeometry />
-          <meshStandardMaterial color="royalblue" />
-        </mesh>
-      </Canvas>
-    </>
-  );
+  return <TestPage />;
 }
 
 export default App;
